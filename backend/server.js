fb125eb1138b858392d96c0c293c0cfe4196d7f8
TDMeyers <meyers.dylan@outlook.com>
2023-08-28 14:30:08 -0600

    began DB configuration

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+// Import and require dotenv
+require('dotenv').config()
+
+// Import and require Express
+const express = require('express')
+
+// Import and require CORS
+const cors = require('cors')
+
+// Setup Express and port
+const app = express()
+
+const PORT = 8080
+
+// Import and require connectDB
+const connectDB = require('./config')
+
+// Connect to the database
+connectDB()
+
+app.use(express.json())
+app.use(cors())
+
+
+
+// Listen to the configured port
+app.listen(PORT, () => {
+    console.log('Listening to PORT: ' + PORT)
+})
