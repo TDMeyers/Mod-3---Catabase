340b009862b25e4e6934acb241fa0bb2a48e45d2
TDMeyers <meyers.dylan@outlook.com>
2023-09-01 13:48:02 -0600

    setup mapping of api call

--- client/src/pages/cats/Index.jsx
+++ client/src/pages/cats/Index.jsx
@@ -5,29 +5,54 @@ import axios from '../../api'
 
 function Index({ user }) {
 
-    const [cats, setCats] = useState([])
+    const [breeds, setBreeds] = useState([])
+
+    const [pics, setPics] = useState([])
 
     const navigate = useNavigate()
 
-    const url = `https://api.thecatapi.com/v1/breeds`;
-
-    let storedBreeds = []
-
     async function getBreeds() {
+
+        const breedsUrl = `https://api.thecatapi.com/v1/breeds`;
+
+        
+
         try {
             console.log('v1.00')
-            const response = await axios.get(url,
+            const response = await axios.get(breedsUrl,
                 {
-                    headers: { "X-Api-Key": import.meta.env.VITE_APP_THE_CAT_API }
+                    headers: { "x-api-key": import.meta.env.VITE_APP_THE_CAT_API }
                 }
             );
-            storedBreeds = response.data.filter(img => img.image?.url != null)
-
-            setCats(storedBreeds)
+            console.log(response.data)
+            setBreeds(response.data)
 
         } catch (err) {
             console.log(err)
         }
+    }
+
+    async function getPics({obj}) {
+
+        const picsUrl = 'https://api.thecatapi.com/v1/images/search';
+
+        const filteredPics = [{obj}]
+
+        try {
+            console.log('v1.00')
+            const filteredPics = await axios.get(picsUrl,
+                {
+                    headers: { "x-api-key": import.meta.env.VITE_APP_THE_CAT_API }
+                }
+            );
+
+            console.log(filteredPics)
+        } catch (err) {
+            console.log(err)
+        }
+
+        console.log(filteredPics)
+        setPics(filteredPics)
     }
 
     useEffect(() => {
@@ -38,17 +63,16 @@ function Index({ user }) {
         <>
             <h1>Cats! Cats! Cats!</h1>
             <div id="cats">
-
-                {cats.map((cat) =>
-                    <div className="a-cat" key={cat.id}>
-                        <img src={cat.vetstreet_url} alt="" />
-                        </div>
+                {breeds.map((breed) =>
+                    <div className="each-breed" key={breed.id}>
+                        <h3>{breed.name}</h3>
+                        <img src={} />
+                    </div>
                 )}
 
-
-                {user &&
+                {/* {user &&
                     <button onClick={() => navigate('/cats/new')}>NEW cat</button>
-                }
+                } */}
 
             </div>
         </>
