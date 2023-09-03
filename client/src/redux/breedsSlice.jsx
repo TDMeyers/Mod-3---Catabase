01e81173f9ba3a1f1e0479e6010116631cdd276b
TDMeyers <meyers.dylan@outlook.com>
2023-09-03 12:56:09 -0600

    added breedsSlice

--- client/src/redux/breedsSlice.jsx
+++ client/src/redux/breedsSlice.jsx
@@ -1,5 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const initialState = {
+    breeds: [],
+    pics: [],
+
+}
 
 const breedSlice = createSlice({
-    
-})
+    name: 'breeds',
+    initialState,
+    reducers: {
+        fetchBreeds: async (state, action) => {
+            try {
+                const breedsUrl = 'https://api.thecatapi.com/v1/breeds';
+                const response = await axios.get(breedsUrl, {
+                    headers: { 'x-api-key': import.meta.env.VITE_APP_THE_CAT_API },
+                });
+                state.breeds = response.data;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        fetchPics: async (state, action) => {
+            try {
+                const picUrl = `https://api.thecatapi.com/v1/images/${action.payload.reference_image_id}?size=thumb`;
+                const response = await axios.get(picUrl, {
+                    headers: { 'x-api-key': import.meta.env.VITE_APP_THE_CAT_API },
+                });
+                state.pics.push(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+});
+
+export const { fetchBreeds, fetchPics } = breedSlice.actions;
+export default breedSlice.reducer;
--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './userSlice'
+import breedsReducer from "./breedsSlice"
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        breeds: breedsReducer,
     },
 });
