aa9741f2b040c88d18b009fce1b7bae9c556d05b
TDMeyers <meyers.dylan@outlook.com>
2023-09-04 13:15:24 -0600

    fixed profile logic and component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -10,37 +10,43 @@ export default function Profile() {
     const [favsInfo, setFavsInfo] = useState([]);
     const [input, setInput] = useState("");
     const [isLoading, setIsLoading] = useState(true);
+    const [idToBreedInfo, setIdToBreedInfo] = useState({}); // Declare the mapping object
 
-    async function getFavs() {
+    async function getFavsAndInfo() {
         try {
             const response = await axios.get("/api/cats", {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
             });
-            setFavs(response.data);
-            console.log(response.data);
+            const favsData = response.data;
+
+            // Fetch breed info for each fav
+            const favInfoPromises = favsData.map(async (fav) => {
+                const favInfoResponse = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${fav.breed}`, {
+                    headers: { "x-api-key": import.meta.env.VITE_APP_THE_CAT_API },
+                });
+                const favInfo = favInfoResponse.data[0]; // Assuming each fav has only one info entry
+                return favInfo;
+            });
+
+            const favInfoData = await Promise.all(favInfoPromises);
+
+            // Create a mapping object to associate _id values with breed info
+            const idToBreedInfo = {};
+            favsData.forEach((fav, index) => {
+                idToBreedInfo[fav._id] = favInfoData[index];
+            });
+
+            setFavs(favsData);
+            setFavsInfo(favInfoData);
+            setIdToBreedInfo(idToBreedInfo); // Store the mapping object in state
         } catch (err) {
             console.log(err);
         } finally {
             setIsLoading(false);
         }
     }
-
-    async function getFavsInfo(breedId) {
-        const favInfoUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-        try {
-            const response = await axios.get(favInfoUrl, {
-                headers: { "x-api-key": import.meta.env.VITE_APP_THE_CAT_API },
-            });
-            const newFavInfo = response.data;
-            console.log(response.data)
-            setFavsInfo(newFavInfo); // Set the new data, replacing the old data
-        } catch (err) {
-            console.log(err);
-        }
-    }
-    
 
     async function handleDeleteFav(favId) {
         try {
@@ -70,19 +76,14 @@ export default function Profile() {
     useEffect(() => {
         setFavs([]);
         setFavsInfo([]);
-        getFavs();
+        if (favs.length === 0) {
+            // Only fetch favs if favs array is empty
+            getFavsAndInfo();
+        }
     }, []);
 
-    useEffect(() => {
-        if (favs.length > 0) {
-            favs.forEach((fav) => {
-                getFavsInfo(fav.breed);
-            });
-        }
-    }, [favs]);
-
     return (
-        <>
+        <div>
             <div className="user-profile">
                 <img src={image} alt="Profile image" />
                 <h1>{username}</h1>
@@ -98,19 +99,22 @@ export default function Profile() {
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : (
-                    favsInfo.map((fav, index) => (
-                        <div className="a-cat" key={index}>
-                            {/* {console.log(fav)} */}
-                            <h3>{fav.breed}</h3>
-                            <h3>{fav.id}</h3>
-                            <div className="breed-image">
-                                <img src={fav.url} alt={fav.id} />
+                    <div className="fav-card">
+                        {favsInfo.map((fav, index) => (
+                            <div className="a-cat" key={index}>
+                                <div className="fav-image">
+                                    <img src={fav.url} alt={fav.id} />
+                                </div>
+                                <div className="fav-info">
+                                    <h3>{fav.breed}</h3>
+                                    <h3>{fav.id}</h3>
+                                </div>
+                                <button onClick={() => handleDeleteFav(fav._id)}>Re-meow-ve Favorite</button>
                             </div>
-                            <button onClick={() => handleDeleteFav(fav._id)}>Re-meow-ve Favorite</button>
-                        </div>
-                    ))
+                        ))}
+                    </div>
                 )}
             </div>
-        </>
+        </div>
     );
 }
