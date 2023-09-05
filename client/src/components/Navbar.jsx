8e2828a37ed6f3128545fbe9d2289e88ff1f2203
TDMeyers <meyers.dylan@outlook.com>
2023-09-05 11:16:40 -0600

    styled navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom";
 import { addUser } from "../redux/userSlice";
 import { useDispatch, useSelector } from "react-redux";
-
-
-
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
 
 function Navbar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     const user = useSelector(state => state.user)
     const dispatch = useDispatch()
 
@@ -14,34 +43,155 @@ function Navbar() {
         dispatch(addUser({}))
     };
 
+    const isLoggedIn = !!user.token; 
+
+    const pageLinks = isLoggedIn
+        ? [
+            { label: 'Saved Cats', path: '/profile' }
+        ]
+    : [{ label: 'Catjam', path: '/catjam' }]
+
+    // const pages = [
+    //     { label: 'Saved Cats', path: '/profile' },
+    //     { label: 'Catjam', path: '/catjam' }
+    // ];
+
+    const settingsLinks = isLoggedIn
+        ? [
+            { label: 'Profile', path: '/profile' },
+            { label: 'Sign out', onClick: logout  }
+        ]
+    : [{ label: 'Register', path: '/register'}]
+    
+    // const settings = [
+    //     { label: 'Profile', path: '/profile' },
+    //     { label: 'Sign out', onClick: logout  }
+    // ]
+
     return (
-        <ul id='navbar'>
-            <li>
-                <Link to="/">
-                    <img src="" alt="React Icon" id="icon" />
-                </Link>
-            </li>
-            {user.username ?
-                <>
-                    <li style={{ color: "white" }}>Welcome {user.username}!</li>
-                    <li onClick={logout}>
-                        <Link to="/cats">Logout</Link>
-                    </li>
-                    <li>
-                        <Link to='/profile'>Profile</Link>
-                    </li>
-                </>
-                :
-                <>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/register">Register</Link>
-                    </li>
-                </>
-            }
-        </ul>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AllInclusiveIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component={Link}
+                        to="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        CAT-A-BASE
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pageLinks.map((page) => (
+                                <MenuItem key={page.label} onClick={handleCloseNavMenu} component={Link} to={page.path}>
+                                    <Typography textAlign="center">{page.label}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AllInclusiveIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component={Link}
+                        to="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        CAT-A-BASE
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pageLinks.map((page) => (
+                            <Button
+                                key={page.label}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                                component={Link}
+                                to={page.path}
+                            >
+                                {page.label}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="D & D Logo" src="" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settingsLinks.map((setting, index) => (
+                                <MenuItem key={index} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting.label}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
